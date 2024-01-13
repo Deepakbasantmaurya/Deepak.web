2267c465afd25d935ff72e252a75c144fefb22b5
DEEPAK MAURYA <146301775+Deepakbasantmaurya@users.noreply.github.com>
2024-01-14 04:06:39 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -1,9 +1,9 @@
-burger=document.querySelector(".burger");
- navbar=document.querySelector(".navbar");
- rightNav=document.querySelector(".rightNav");
- navlist=document.querySelector(".nav-list");
-burger.addEventListener('click',()=>{
-    rightNav.classList.toggle('v-class');
-    navlist.classList.toggle('v-class');
-    navbar.classList.toggle('h-class');
+  burger=document.querySelector('.burger')
+  navbar=document.querySelector('.navbar')
+  rightNav=document.querySelector('.rightNav')
+  navlist=document.querySelector('.nav-list')
+burger.addEventListener("click",()=>{
+    rightNav.classList.toggle('v-nav-resp')
+    navlist.classList.toggle('v-nav-resp')
+    navbar.classList.toggle('h-nav-resp')
 })
